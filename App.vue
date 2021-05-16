31958b60bf75302c14dcb1343d1d95f9f39fc371
仲間太陽 <taiyoonakama@gmail.com>
2021-05-17 07:18:09 +0900

    second-commit

--- App.vue
+++ App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <router-link to="/">トップページ</router-link>
-    <router-link to="/user-list">ユーザー一覧ページ</router-link>
+    <router-link v-bind:to="{name:'Home'}">トップページ</router-link>
+    <router-link v-bind:to="{name:'UserList'}">ユーザー一覧ページ</router-link>
     <router-view></router-view>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import UserList　from '../views/UserList.vue'
+import UserList from '../views/UserList.vue'
 
 Vue.use(VueRouter)
 
@@ -15,9 +15,6 @@ const routes = [
     path: '/user-list',
     name: 'UserList',
     component: UserList
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
   }
 ]
 
